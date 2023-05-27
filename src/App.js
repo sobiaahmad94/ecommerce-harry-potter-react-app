2f9134a5288ad28784285142dfd9f74dc3106c3d
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-27 01:41:46 +0100

    error page react route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import ProductsPage from './components/ProductsPage';
 import CheckOutPage from './components/CheckOutPage';
+import ErrorPage from './components/ErrorPage';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           <Route exact path="/home" element={<HomePage />}></Route>
           <Route exact path="/products" element={<ProductsPage />}></Route>
           <Route exact path="/check-out" element={<CheckOutPage />}></Route>
+          <Route exact path="/*" element={<ErrorPage />}></Route>
         </Routes>
 
 
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ErrorPage() {
+    return (
+        <>
+        <h1>404 Error</h1>
+        <p>Unfortunately, the page you're attempting to visit doesn't exist.</p>
+        </>
+
+    );
+}
+
+export default ErrorPage;
